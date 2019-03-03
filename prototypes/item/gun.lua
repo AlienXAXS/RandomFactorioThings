56bbb260d73015c5b0e1db525ce2178e25235a07
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-03-03 16:30:56 +0300

    RIP axe

--- prototypes/item/gun.lua
+++ prototypes/item/gun.lua
@@ -19,34 +19,4 @@ very_long_range_rocket_cannon.attack_parameters.range = 20 * 32
 very_long_range_rocket_cannon.attack_parameters.cooldown = 150
 table.insert(gun, very_long_range_rocket_cannon)
 
---[[{
-    type = "gun",
-    name = "artillery-wagon-cannon",
-    icon = "__base__/graphics/icons/tank-cannon.png",
-    icon_size = 32,
-    flags = {"goes-to-main-inventory", "hidden"},
-    subgroup = "gun",
-    order = "z[artillery]-a[cannon]",
-    attack_parameters =
-    {
-      type = "projectile",
-      ammo_category = "artillery-shell",
-      cooldown = 200,
-      movement_slow_down_factor = 0,
-      projectile_creation_distance = 1.6,
-      projectile_center = {-0.15625, -0.07812},
-      range = 7 * 32,
-      min_range = 1 * 32,
-      projectile_creation_parameters = require("prototypes.entity.artillery-cannon-muzzle-flash-shifting"),
-      sound =
-      {
-        {
-          filename = "__base__/sound/fight/tank-cannon.ogg",
-          volume = 1.0
-        }
-      }
-    },
-    stack_size = 1
-  }]]
-
 data:extend(gun)
