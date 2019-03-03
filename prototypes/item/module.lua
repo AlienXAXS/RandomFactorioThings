baccbdb32427c058a0125fa6232a0db1fb7d85ce
JohnTheCoolingFan <ivan8215145640@gmail.com>
2019-03-03 17:49:30 +0300

    Removed "goes-to-main-inventory" flag

--- prototypes/item/module.lua
+++ prototypes/item/module.lua
@@ -4,7 +4,7 @@ data:extend({
 		name = "speed-module-4",
 		icon = "__RandomFactorioThings__/graphics/icons/speed-module-4.png",
 		icon_size = 32,
-		flags = {"goes-to-main-inventory"},
+		flags = {},
 		subgroup = "module",
 		category = "speed",
 		tier = 4,
@@ -13,13 +13,13 @@ data:extend({
 		default_request_amount = 10,
 		effect = { speed = {bonus = 0.7}, consumption = {bonus = 1}}
 	},
-  
+
 	{
 		type = "module",
 		name = "effectivity-module-4",
 		icon = "__RandomFactorioThings__/graphics/icons/effectivity-module-4.png",
 		icon_size = 32,
-		flags = {"goes-to-main-inventory"},
+		flags = {},
 		subgroup = "module",
 		category = "effectivity",
 		tier = 4,
@@ -29,20 +29,20 @@ data:extend({
 		effect = { consumption = {bonus = -0.7}},
 		limitation = production
 	},
-  
+
 	{
 		type = "module",
 		name = "productivity-module-4",
 		icon = "__RandomFactorioThings__/graphics/icons/productivity-module-4.png",
 		icon_size = 32,
-		flags = {"goes-to-main-inventory"},
+		flags = {},
 		subgroup = "module",
 		category = "productivity",
 		tier = 4,
 		order = "c[productivity]-d[productivity-module-4]",
 		stack_size = 50,
 		default_request_amount = 10,
-		effect = 
+		effect =
 		{
 			productivity = {bonus = 0.12},
 			consumption = {bonus = 1},
